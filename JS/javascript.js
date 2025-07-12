5a759ab944ad0028a00f3893195eae4574fd2d98
Maksim Padrabinkin <132061563+maxFadet@users.noreply.github.com>
2025-07-12 19:10:41 +0200

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -3,6 +3,7 @@
     console.log("Hi everybody who has come here");
   };
 
+
   const switchBackground = () => {
   const bodyStyle = document.querySelector(".body");
   const toggleCircle = document.querySelector(".js-toggleCircle");

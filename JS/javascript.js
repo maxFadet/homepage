af92945e2b984af18f75ad8e88cd121a54aa48e3
Maksim Padrabinkin <132061563+maxFadet@users.noreply.github.com>
2025-07-26 22:03:54 +0200

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -44,6 +44,7 @@
     const targetId = event.target.getAttribute("href").substring(1);
     const targetSection = document.getElementById(targetId);
 
+
     if (targetSection) {
       targetSection.scrollIntoView({
         behavior: "smooth",
